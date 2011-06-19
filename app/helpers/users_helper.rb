8446336c45c62a51fa2143315e78d143e2c246e6
Terry <terry_lee_welch@hotmail.com>
2011-06-19 14:22:55 +0000

    Users complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,8 @@ module UsersHelper
 
   def gravatar_for(user, options = { :size => 50 })
     gravatar_image_tag(user.email.downcase, :alt => "No image",
-                                         :class => 'gravatar',
-                                       :gravatar => options)
+                                            :class => 'gravatar',
+                                            :gravatar => options)
  # gravatar_image_tag(user.email.downcase, :class => 'gravatar')
   end
 end
